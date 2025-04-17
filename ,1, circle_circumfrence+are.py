c3b6ae8e89b9f67f03146e8b1e2e516477464b0d
Aayush08102013 <aayush.ranjan0815@gmail.com>
2025-04-17 16:42:23 +1200

    Add files via upload

---
+++ ,1, circle_circumfrence+are.py
@@ -0,0 +1,37 @@
+# Importing Math:
+import math
+
+# Take input from the user
+r = float(input("Enter your radius: "))
+
+# Calculate the circumfrence of the circle
+total = 2 * r * math.pi
+
+#print the total circumfrence:
+print(f"The circumfrence of your circle is {total:.3f}")
+
+
+# finding the area of a circle:
+# entering radius:
+radius = float(input("Enter your radius: "))
+
+# Figuring out the area:
+
+a = math.pi * (radius ** 2)
+# printing the result:
+
+print(f"The area of your circle is {a:.3f}")
+
+# Finding the Diameter of a circle:
+Radius = float(input("Enter your radius: "))
+
+# Calculating the diameter:
+d = 2 * Radius
+
+#Printing the answer:
+print(f"Your diameter of your circle is {d:.3f}")
+
+
+
+        
+
